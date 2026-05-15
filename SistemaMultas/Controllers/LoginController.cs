92183e3c97e4d733197c79672d06c9e0b7b5483d
jjimenez2022152 <jjimenez-2022152@kinal.edu.gt>
2026-05-15 01:10:38 -0600

    Proyecto completo Login

---
+++ SistemaMultas/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using SistemaMultas.Models.Login;
+using static SistemaMultas.Models.Login.csEstructuraLogin;
+
+namespace SistemaMultas.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("rest/api/loginUsuario")]
+        public responseLogin loginUsuario(requestLogin model)
+        {
+            csLogin login = new csLogin();
+
+            return login.loginUsuario(
+                model.username,
+                model.password
+            );
+        }
+    }
+}
---
+++ SistemaMultas/Models/Login/csLogin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SistemaMultas.Models.Login
+{
+    public class csLogin
+    {
+        public csEstructuraLogin.responseLogin loginUsuario(string username, string password)
+        {
+            csEstructuraLogin.responseLogin result =
+                new csEstructuraLogin.responseLogin();
+
+            string conexion = "";
+            SqlConnection con = null;
+
+            try
+            {
+                conexion = ConfigurationManager
+                    .ConnectionStrings["cnConnection"]
+                    .ConnectionString;
+
+                con = new SqlConnection(conexion);
+
+                con.Open();
+
+                string cadena =
+                    "SELECT * FROM Usuarios " +
+                    "WHERE username = '" + username + "' " +
+                    "AND password = '" + password + "'";
+
+                SqlCommand cmd = new SqlCommand(cadena, con);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    result.respuesta = 1;
+                    result.descripcion_respuesta = "Login correcto";
+                }
+                else
+                {
+                    result.respuesta = 0;
+                    result.descripcion_respuesta = "Usuario o contraseña incorrectos";
+                }
+            }
+            catch (Exception ex)
+            {
+                result.respuesta = 0;
+                result.descripcion_respuesta = ex.Message;
+            }
+
+            if (con != null)
+                con.Close();
+
+            return result;
+        }
+    }
+}
